e177f42bdf4e16fe6dd748a37caad0b540d68c8e
argaed <edgar.arana@operacionesdelnorte.com>
2017-08-02 10:31:34 -0500

    spash

---
+++ app/src/main/java/com/lionsquare/evolutionflag/activities/CategoryActivity.java
@@ -0,0 +1,15 @@
+package com.lionsquare.evolutionflag.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lionsquare.evolutionflag.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
--- app/src/main/java/com/lionsquare/evolutionflag/activities/SplashScreenActivity.java
+++ app/src/main/java/com/lionsquare/evolutionflag/activities/SplashScreenActivity.java
@@ -2,49 +2,81 @@ package com.lionsquare.evolutionflag.activities;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.lionsquare.evolutionflag.R;
+import com.lionsquare.evolutionflag.databinding.ActivitySplashScreenBinding;
+import com.lionsquare.evolutionflag.utils.MyBounceInterpolator;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Set the duration of the splash screen
-    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    ActivitySplashScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set portrait orientation
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         // Hide title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen);
 
-        setContentView(R.layout.activity_splash_screen);
 
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
+        binding.ascIvLogo.setOnClickListener(this);
 
-                // Start the next activity
-                Intent mainIntent = new Intent().setClass(
-                        SplashScreenActivity.this, MainActivity.class);
-                startActivity(mainIntent);
+        animateButton();
 
-                // Close the activity so the user won't able to go back this
-                // activity pressing Back button
-                finish();
-            }
-        };
-
-        // Simulate a long loading process on application startup.
-        Timer timer = new Timer();
-        timer.schedule(task, SPLASH_SCREEN_DELAY);
     }
 
+    @Override
+    public void onClick(View v) {
+        v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent i = new Intent(this, MenuActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    void animateButton() {
+        // Load the animation
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        double animationDuration = 2.0 * 1000;
+        myAnim.setDuration((long) animationDuration);
+
+        // Use custom animation interpolator to achieve the bounce effect
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.20, 20.0);
+
+        myAnim.setInterpolator(interpolator);
+
+        // Animate the button
+        binding.ascIvLogo.startAnimation(myAnim);
+        //playSound();
+
+        // Run button animation again after it finished
+        myAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation arg0) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                animateButton();
+            }
+        });
+    }
 }
